556a55732b160541e44978a3270578fabe07b3b3
root <root@hlkali.hacking-lab.com>
2020-12-17 08:38:54 +0100

    update openvpn hl2

--- openvpn/down.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mv -f /etc/resolv.conf.old /etc/resolv.conf
-chmod 644 /etc/resolv.conf
--- openvpn/start_openvpn.sh
+++ openvpn/start_openvpn.sh
@@ -1,2 +1,7 @@
 # Start VPN in terminal as root (debugging purpose)
-/usr/sbin/openvpn --cd /etc/openvpn --script-security 2 --config /etc/openvpn/HackingLab.ovpn
+echo "==============================================================================="
+echo "   VPN WILL ONLY WORK IF YOU HAVE PREVIOUSLY AUTHENTICATED IN A HL WEB APP    "
+echo "==============================================================================="
+mydir=`pwd`
+/usr/sbin/openvpn --cd $mydir --script-security 2 --config $mydir/Hacking-Lab-2.0.ovpn
+
